8353561e81d36151be013d5f1248ae0ea2de692d
ElizaManthei <mazilemka@gmail.com>
2022-12-20 22:47:01 +0100

    Gecorrigeerd

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        String letters[] = {"zero", "een", "twee", "drie", "vier", "vijf", "zes", "seven", "acht", "negen"};
+        String letters[] = {"nul", "een", "twee", "drie", "vier", "vijf", "zes", "seven", "acht", "negen"};
 
         Translator transl = new Translator(numbers, letters);
         Scanner userIn = new Scanner(System.in);
